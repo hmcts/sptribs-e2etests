eed399006efb618b6fed227f05db2e3e80af0b40
Jake Gowler <jakecgowler@gmail.com>
2023-12-11 21:40:47 +0000

    WCAG 2.2AA checking update

--- tests/helpers/accessibilityTestHelper.js
+++ tests/helpers/accessibilityTestHelper.js
@@ -3,7 +3,7 @@ const AxeBuilder = require("@axe-core/playwright").default;
 
 async function axeTest(page) {
   const accessibilityScanResults = await new AxeBuilder({ page })
-    .withTags(["wcag2a", "wcag2aa", "wcag21a", "wcag21aa"])
+    .withTags(["wcag2a", "wcag2aa", "wcag21a", "wcag21aa", "wcag22a", "wcag22aa"])
     .analyze();
   await expect(accessibilityScanResults.violations).toEqual([]);
 }
