a06524ddec837f756780ce7e366fa62f5f2a5891
Jake Gowler <jakecgowler@gmail.com>
2023-12-15 10:24:00 +0000

    Flakiness fixing

--- playwright.config.js
+++ playwright.config.js
@@ -7,61 +7,43 @@ module.exports = defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: 2, // Set the number of retries for all projects
+  retries: 10, // Set the number of retries for all projects
 
+  timeout: 3 * 30 * 1000,
   expect: {
-    timeout: 20 * 1000,
+    timeout: 60 * 1000,
   },
 
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
   },
-
-  /* Configure projects for major browsers */
   projects: [
     {
       name: "chromium",
       use: { ...devices["Desktop Chrome"] },
     },
-
     {
       name: "firefox",
       use: { ...devices["Desktop Firefox"] },
     },
-
     {
       name: "webkit",
       use: { ...devices["Desktop Safari"] },
     },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-
+    {
+      name: 'Mobile Chrome',
+      use: { ...devices['Pixel 5'] },
+    },
     {
       name: "MobileSafari",
       use: { ...devices["iPhone 12"] },
     },
-
-    /* Test against branded browsers. */
     {
       name: "MicrosoftEdge",
       use: { ...devices["Desktop Edge"], channel: "msedge" },
     },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
 });
