28e56f7475ffd67cbffc7443db4a716e64c2cc4b
Jake Gowler <jakecgowler@gmail.com>
2023-12-15 16:23:39 +0000

    Flakiness fixing

--- tests/helpers/accessibilityTestHelper.js
+++ tests/helpers/accessibilityTestHelper.js
@@ -3,7 +3,14 @@ const AxeBuilder = require("@axe-core/playwright").default;
 
 async function axeTest(page) {
   const accessibilityScanResults = await new AxeBuilder({ page })
-    .withTags(["wcag2a", "wcag2aa", "wcag21a", "wcag21aa", "wcag22a", "wcag22aa"])
+    .withTags([
+      "wcag2a",
+      "wcag2aa",
+      "wcag21a",
+      "wcag21aa",
+      "wcag22a",
+      "wcag22aa",
+    ])
     .analyze();
   await expect(accessibilityScanResults.violations).toEqual([]);
 }
