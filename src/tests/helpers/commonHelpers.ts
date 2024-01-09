30d3a9cc844874504b3541e7c610dad4ddce71a5
Jake Gowler <jakecgowler@gmail.com>
2024-01-09 15:17:31 +0000

    typing commonHelpers.ts

--- src/tests/helpers/commonHelpers.ts
+++ src/tests/helpers/commonHelpers.ts
@@ -2,57 +2,50 @@ import subjectDetailsPage from "../fixtures/content/DSSCreateCase/SubjectDetails
 
 type CommonHelpers = {
   readonly months: string[];
-  padZero(value: number): Promise<string>;
+  padZero(value: number): string;
   convertDate(tab: string): Promise<string>;
   getTimestamp(): Promise<string>;
 };
 
-const months: string[] = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
+const commonHelpers: CommonHelpers = {
+  months: [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ],
 
-// Add a zero before a value if it is under 10 - used for timestamp
-function padZero(value: number): string | number {
-  return value < 10 ? "0" + value : value;
-}
+  padZero(value: number): string {
+    return value < 10 ? "0" + value : value.toString();
+  },
 
-async function convertDate(tab: string): Promise<string> {
-  const dayOfBirth = subjectDetailsPage.dayOfBirth;
-  const monthOfBirth = subjectDetailsPage.monthOfBirth;
-  const yearOfBirth = subjectDetailsPage.yearOfBirth;
-  const monthName = months[Number(monthOfBirth) - 1];
-  if (tab) {
-    return `${dayOfBirth} ${monthName.slice(0, 3)} ${yearOfBirth}`;
-  } else {
-    return `${dayOfBirth} ${monthName} ${yearOfBirth}`;
-  }
-}
+  async convertDate(tab: string): Promise<string> {
+    const dayOfBirth = subjectDetailsPage.dayOfBirth;
+    const monthOfBirth = subjectDetailsPage.monthOfBirth;
+    const yearOfBirth = subjectDetailsPage.yearOfBirth;
+    const monthName = this.months[Number(monthOfBirth) - 1];
+    if (tab) {
+      return `${dayOfBirth} ${monthName.slice(0, 3)} ${yearOfBirth}`;
+    } else {
+      return `${dayOfBirth} ${monthName} ${yearOfBirth}`;
+    }
+  },
 
-async function getTimestamp(): Promise<string> {
-  const currentDate = new Date();
-  let hours = currentDate.getHours();
-  hours = hours % 12;
-  hours = hours ? hours : 12;
-  return `${currentDate.getDate()} ${months[currentDate.getMonth()].slice(
-    0,
-    3,
-  )} ${currentDate.getFullYear()}, ${hours}:${padZero(
-    currentDate.getMinutes(),
-  )}`;
-}
-
-export default {
-  convertDate,
-  getTimestamp,
+  async getTimestamp(): Promise<string> {
+    const currentDate = new Date();
+    let hours = currentDate.getHours();
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+    return `${currentDate.getDate()} ${this.months[currentDate.getMonth()].slice(0, 3)} ${currentDate.getFullYear()}, ${hours}:${this.padZero(currentDate.getMinutes())}`;
+  },
 };
+
+export default commonHelpers;
