6fc28e6bc3ba0f255c1fa40a2c7afb11a50f824d
Jake Gowler <jakecgowler@gmail.com>
2024-01-10 12:49:12 +0000

    caseFileViewTabPage.ts

---
+++ src/tests/pages/CaseAPI/caseTabs/caseFileViewTabPage.ts
@@ -0,0 +1,130 @@
+import { expect, Page } from "@playwright/test";
+import path from "path";
+import config from "../../../config.ts";
+import axeTest from "../../../helpers/accessibilityTestHelper.ts";
+import allTabTitlesContent from "../../../fixtures/content/CaseAPI/caseTabs/allTabTitles_content.ts";
+import caseFileViewTabContent from "../../../fixtures/content/CaseAPI/caseTabs/caseFileViewTab_content.ts";
+import uploadedDocumentsContent from "../../../fixtures/content/CaseAPI/caseTabs/uploadedDocuments_content.ts";
+
+type CaseFileViewTabPage = {
+  caseFileViewTab: string;
+  checkPageLoads(
+    page: Page,
+    accessibilityTest: boolean,
+    caseNumber: string,
+  ): Promise<void>;
+  changeToCaseFileViewTab(page: Page): Promise<void>;
+  checkPageInfo(page: Page): Promise<void>;
+};
+
+const caseFileViewTabPage: CaseFileViewTabPage = {
+  caseFileViewTab: ".mat-tab-label",
+
+  async checkPageLoads(
+    page: Page,
+    accessibilityTest: boolean,
+    caseNumber: string,
+  ): Promise<void> {
+    await expect(page.locator(".case-field").first()).toContainText(
+      allTabTitlesContent.pageTitle + caseNumber,
+    );
+    await expect(page.locator(".mat-tab-label").nth(0)).toHaveText(
+      allTabTitlesContent.tab1,
+    );
+    await expect(page.locator(".mat-tab-label").nth(1)).toHaveText(
+      allTabTitlesContent.tab2,
+    );
+    await expect(page.locator(".mat-tab-label").nth(2)).toHaveText(
+      allTabTitlesContent.tab3,
+    );
+    await expect(page.locator(".mat-tab-label").nth(3)).toHaveText(
+      allTabTitlesContent.tab4,
+    );
+    await expect(page.locator(".mat-tab-label").nth(4)).toHaveText(
+      allTabTitlesContent.tab5,
+    );
+    await expect(page.locator(".mat-tab-label").nth(5)).toHaveText(
+      allTabTitlesContent.tab6,
+    );
+    await expect(page.locator(".mat-tab-label").nth(6)).toHaveText(
+      allTabTitlesContent.tab7,
+    );
+    await expect(page.locator(".mat-tab-label").nth(7)).toHaveText(
+      allTabTitlesContent.tab8,
+    );
+    await expect(page.locator(".mat-tab-label").nth(8)).toHaveText(
+      allTabTitlesContent.tab9,
+    );
+    await expect(page.locator(".mat-tab-label").nth(9)).toHaveText(
+      allTabTitlesContent.tab10,
+    );
+    await expect(page.locator(".mat-tab-label").nth(10)).toHaveText(
+      allTabTitlesContent.tab11,
+    );
+    await expect(page.locator(".mat-tab-label").nth(11)).toHaveText(
+      allTabTitlesContent.tab12,
+    );
+    await expect(page.locator(".mat-tab-label").nth(12)).toHaveText(
+      allTabTitlesContent.tab13,
+    );
+    await expect(page.locator(".mat-tab-label").nth(13)).toHaveText(
+      allTabTitlesContent.tab14,
+    );
+    await expect(page.locator(".govuk-heading-l")).toHaveText(
+      caseFileViewTabContent.pageTitle,
+    );
+    await expect(page.locator(".node__name").nth(0)).toHaveText(
+      caseFileViewTabContent.textOnPage1,
+    );
+    await expect(page.locator(".node__name").nth(1)).toHaveText(
+      caseFileViewTabContent.textOnPage2,
+    );
+    await expect(page.locator(".node__name").nth(2)).toHaveText(
+      caseFileViewTabContent.textOnPage3,
+    );
+    await expect(page.locator(".node__name").nth(3)).toHaveText(
+      caseFileViewTabContent.textOnPage4,
+    );
+    await expect(page.locator(".node__name").nth(4)).toHaveText(
+      caseFileViewTabContent.textOnPage5,
+    );
+    await expect(page.locator(".node__name").nth(7)).toHaveText(
+      caseFileViewTabContent.textOnPage6,
+    );
+    await expect(page.locator(".node__name").nth(8)).toHaveText(
+      caseFileViewTabContent.textOnPage7,
+    );
+    await expect(page.locator(".node__name").nth(9)).toHaveText(
+      caseFileViewTabContent.textOnPage8,
+    );
+    await expect(page.locator(".node__name").nth(10)).toHaveText(
+      caseFileViewTabContent.textOnPage9,
+    );
+    await expect(page.locator(".node__name").nth(11)).toHaveText(
+      caseFileViewTabContent.textOnPage10,
+    );
+
+    if (accessibilityTest) {
+      await axeTest(page);
+    }
+  },
+
+  async changeToCaseFileViewTab(page: Page): Promise<void> {
+    await page.locator(this.caseFileViewTab).nth(9).click();
+    await page.locator(this.caseFileViewTab).nth(9).click();
+  },
+
+  async checkPageInfo(page: Page): Promise<void> {
+    await expect(page.locator(".node__count").nth(4)).toHaveText(
+      uploadedDocumentsContent.totalDocuments,
+    );
+    await expect(page.locator(".node__name").nth(5)).toHaveText(
+      path.basename(config.testFile),
+    );
+    await expect(page.locator(".node__name").nth(6)).toHaveText(
+      path.basename(config.testPdfFile),
+    );
+  },
+};
+
+export default caseFileViewTabPage;
