a4c6386cb98c117872080ce2e3d58f28bf65109b
Jake Gowler <jakecgowler@gmail.com>
2024-01-24 17:00:22 +0000

    Renaming values

--- src/tests/fixtures/content/CaseAPI/createCase/caseCategorisationDetails_content.ts
+++ src/tests/fixtures/content/CaseAPI/createCase/caseCategorisationDetails_content.ts
@@ -1,8 +1,8 @@
-const caseFilter_content = {
+const caseCategorisationDetails_content = {
   pageTitle: "Create Case",
   textOnPage1: "Jurisdiction",
   textOnPage2: "Case type",
   textOnPage3: "Event",
 } as const;
 
-export default caseFilter_content;
+export default caseCategorisationDetails_content;
