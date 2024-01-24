c872d7939992a76d4be0d2abc487f1713a2723cc
Jake Gowler <jakecgowler@gmail.com>
2024-01-24 17:39:41 +0000

    casedateObjects_content.ts addition

---
+++ src/tests/fixtures/content/CaseAPI/createCase/casedateObjects_content.ts
@@ -0,0 +1,10 @@
+const caseDateObjects_content = {
+  pageHint: "Create Case",
+  pageTitle: "When was the case received?",
+  textOnPage1: "Date of receipt",
+  textOnPage2: "Day",
+  textOnPage3: "Month",
+  textOnPage4: "Year",
+} as const;
+
+export default caseDateObjects_content;
