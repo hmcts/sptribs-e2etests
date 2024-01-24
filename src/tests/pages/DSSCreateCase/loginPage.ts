56326261676477af26efd4a318db769a920fdaad
Jake Gowler <jakecgowler@gmail.com>
2024-01-24 15:18:36 +0000

    Cleanup

--- src/tests/pages/DSSCreateCase/loginPage.ts
+++ src/tests/pages/DSSCreateCase/loginPage.ts
@@ -3,21 +3,10 @@ import idamLoginHelper from "../../helpers/idamLoginHelper.ts";
 import config from "../../config.ts";
 
 type SignInPage = {
-  fields: {
-    username: string;
-    password: string;
-  };
-  submitButton: string;
   SignInUser(page: Page): Promise<void>;
 };
 
 const signInPage: SignInPage = {
-  fields: {
-    username: "#username",
-    password: "#password",
-  },
-  submitButton: 'input[value="Sign in"]',
-
   async SignInUser(page: Page): Promise<void> {
     await page.waitForSelector(
       `#skiplinktarget:text("Sign in or create an account")`,
