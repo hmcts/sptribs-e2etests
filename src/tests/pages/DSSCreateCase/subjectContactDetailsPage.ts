be1b04fe8c4c83f8e8628b917947baf8fac0941f
jessieharrigan <144362161+jessieharrigan@users.noreply.github.com>
2024-01-29 11:15:06 +0000

    Added checking for Welsh to subject pages

--- src/tests/pages/DSSCreateCase/subjectContactDetailsPage.ts
+++ src/tests/pages/DSSCreateCase/subjectContactDetailsPage.ts
@@ -10,9 +10,9 @@ type SubjectContactDetailsPage = {
   contactAgreeBox: string;
   continueButton: string;
   backButton: string;
-  checkPageLoads(page: Page, accessibilityTest: boolean): Promise<void>;
+  checkPageLoads(page: Page, welsh: boolean, accessibilityTest: boolean): Promise<void>;
   fillInFields(page: Page): Promise<void>;
-  triggerErrorMessages(page: Page): Promise<void>;
+  triggerErrorMessages(page: Page, welsh: boolean): Promise<void>;
   pressBackButton(page: Page): Promise<void>;
 };
 
@@ -26,22 +26,44 @@ const subjectContactDetailsPage: SubjectContactDetailsPage = {
   continueButton: "#main-form-submit",
   backButton: ".govuk-back-link",
 
-  async checkPageLoads(page: Page, accessibilityTest: boolean) {
-    await expect(page.locator(".govuk-heading-l")).toHaveText(
-      subjectContactDetailsContent.pageTitle,
-    );
-    await expect(
-      page.locator("main[id='main-content'] p[class='govuk-body']"),
-    ).toHaveText(subjectContactDetailsContent.textOnPage1);
-    await expect(page.locator(".govuk-label").nth(0)).toHaveText(
-      subjectContactDetailsContent.subHeading1,
-    );
-    await expect(page.locator(".govuk-label").nth(1)).toHaveText(
-      subjectContactDetailsContent.subHeading2,
-    );
-    await expect(page.locator("label[for='subjectAgreeContact']")).toHaveText(
-      subjectContactDetailsContent.textOnPage2,
-    );
+  async checkPageLoads(page: Page, welsh: boolean, accessibilityTest: boolean) {
+    switch (welsh) {
+      case true:
+        await expect(page.locator(".govuk-link.language")).toHaveText("English");
+        await expect(page.locator(".govuk-heading-l")).toHaveText(
+          subjectContactDetailsContent.welshPageTitle,
+        );
+        await expect(
+          page.locator("main[id='main-content'] p[class='govuk-body']"),
+        ).toHaveText(subjectContactDetailsContent.welshTextOnPage1);
+        await expect(page.locator(".govuk-label").nth(0)).toHaveText(
+          subjectContactDetailsContent.welshSubHeading1,
+        );
+        await expect(page.locator(".govuk-label").nth(1)).toHaveText(
+          subjectContactDetailsContent.welshSubHeading2,
+        );
+        await expect(page.locator("label[for='subjectAgreeContact']")).toHaveText(
+          subjectContactDetailsContent.welshTextOnPage2,
+        );
+        break;
+      case false:
+        await expect(page.locator(".govuk-heading-l")).toHaveText(
+          subjectContactDetailsContent.pageTitle,
+        );
+        await expect(
+          page.locator("main[id='main-content'] p[class='govuk-body']"),
+        ).toHaveText(subjectContactDetailsContent.textOnPage1);
+        await expect(page.locator(".govuk-label").nth(0)).toHaveText(
+          subjectContactDetailsContent.subHeading1,
+        );
+        await expect(page.locator(".govuk-label").nth(1)).toHaveText(
+          subjectContactDetailsContent.subHeading2,
+        );
+        await expect(page.locator("label[for='subjectAgreeContact']")).toHaveText(
+          subjectContactDetailsContent.textOnPage2,
+        );
+        break;
+    }
     if (accessibilityTest) {
       await axeTest(page);
     }
@@ -60,41 +82,81 @@ const subjectContactDetailsPage: SubjectContactDetailsPage = {
     await page.click(this.continueButton);
   },
 
-  async triggerErrorMessages(page: Page) {
-    await page.click(this.continueButton);
-    await expect(page.locator(".govuk-error-summary__title")).toHaveText(
-      subjectContactDetailsContent.errorBanner,
-    );
-    await expect(page.locator("[href='#subjectEmailAddress']")).toHaveText(
-      subjectContactDetailsContent.validEmailError,
-    );
-    await expect(page.locator("[href='#subjectContactNumber']")).toHaveText(
-      subjectContactDetailsContent.validContactNumberError,
-    );
-    await expect(page.locator("[href='#subjectAgreeContact']")).toHaveText(
-      subjectContactDetailsContent.agreeError,
-    );
-    await expect(page.locator("#subjectEmailAddress-error")).toContainText(
-      subjectContactDetailsContent.validEmailError,
-    );
-    await expect(page.locator("#subjectContactNumber-error")).toContainText(
-      subjectContactDetailsContent.validContactNumberError,
-    );
-    await expect(page.locator("#subjectAgreeContact-error")).toContainText(
-      subjectContactDetailsContent.agreeError,
-    );
-    await page.fill(
-      this.fields.email,
-      subjectContactDetailsContent.partEmailEntry,
-    );
-    await page.click(this.continueButton);
-    await expect(page.locator("[href='#subjectEmailAddress']")).toHaveText(
-      subjectContactDetailsContent.partEmailError,
-    );
-    await expect(page.locator("#subjectEmailAddress-error")).toContainText(
-      subjectContactDetailsContent.partEmailError,
-    );
-    await page.fill(this.fields.email, "");
+  async triggerErrorMessages(page: Page, welsh: boolean) {
+    switch (welsh) {
+      case true:
+        await page.click(this.continueButton);
+        await expect(page.locator(".govuk-error-summary__title")).toHaveText(
+          subjectContactDetailsContent.welshErrorBanner,
+        );
+        await expect(page.locator("[href='#subjectEmailAddress']")).toHaveText(
+          subjectContactDetailsContent.welshValidEmailError,
+        );
+        await expect(page.locator("[href='#subjectContactNumber']")).toHaveText(
+          subjectContactDetailsContent.welshValidContactNumberError,
+        );
+        await expect(page.locator("[href='#subjectAgreeContact']")).toHaveText(
+          subjectContactDetailsContent.welshAgreeError,
+        );
+        await expect(page.locator("#subjectEmailAddress-error")).toContainText(
+          subjectContactDetailsContent.welshValidEmailError,
+        );
+        await expect(page.locator("#subjectContactNumber-error")).toContainText(
+          subjectContactDetailsContent.welshValidContactNumberError,
+        );
+        await expect(page.locator("#subjectAgreeContact-error")).toContainText(
+          subjectContactDetailsContent.welshAgreeError,
+        );
+        await page.fill(
+          this.fields.email,
+          subjectContactDetailsContent.partEmailEntry,
+        );
+        await page.click(this.continueButton);
+        await expect(page.locator("[href='#subjectEmailAddress']")).toHaveText(
+          subjectContactDetailsContent.welshPartEmailError,
+        );
+        await expect(page.locator("#subjectEmailAddress-error")).toContainText(
+          subjectContactDetailsContent.welshPartEmailError,
+        );
+        await page.fill(this.fields.email, "");
+        break;
+      case false:
+        await page.click(this.continueButton);
+        await expect(page.locator(".govuk-error-summary__title")).toHaveText(
+          subjectContactDetailsContent.errorBanner,
+        );
+        await expect(page.locator("[href='#subjectEmailAddress']")).toHaveText(
+          subjectContactDetailsContent.validEmailError,
+        );
+        await expect(page.locator("[href='#subjectContactNumber']")).toHaveText(
+          subjectContactDetailsContent.validContactNumberError,
+        );
+        await expect(page.locator("[href='#subjectAgreeContact']")).toHaveText(
+          subjectContactDetailsContent.agreeError,
+        );
+        await expect(page.locator("#subjectEmailAddress-error")).toContainText(
+          subjectContactDetailsContent.validEmailError,
+        );
+        await expect(page.locator("#subjectContactNumber-error")).toContainText(
+          subjectContactDetailsContent.validContactNumberError,
+        );
+        await expect(page.locator("#subjectAgreeContact-error")).toContainText(
+          subjectContactDetailsContent.agreeError,
+        );
+        await page.fill(
+          this.fields.email,
+          subjectContactDetailsContent.partEmailEntry,
+        );
+        await page.click(this.continueButton);
+        await expect(page.locator("[href='#subjectEmailAddress']")).toHaveText(
+          subjectContactDetailsContent.partEmailError,
+        );
+        await expect(page.locator("#subjectEmailAddress-error")).toContainText(
+          subjectContactDetailsContent.partEmailError,
+        );
+        await page.fill(this.fields.email, "");
+        break;
+    }
   },
 
   async pressBackButton(page) {
--- src/tests/pages/DSSCreateCase/subjectDetailsPage.ts
+++ src/tests/pages/DSSCreateCase/subjectDetailsPage.ts
@@ -11,9 +11,9 @@ type SubjectDetailsPage = {
   };
   continueButton: string;
   rejectCookiesButton: string;
-  checkPageLoads(page: Page, accessibilityTest: boolean): Promise<void>;
+  checkPageLoads(page: Page, welsh: boolean, accessibilityTest: boolean): Promise<void>;
   fillInFields(page: Page): Promise<void>;
-  triggerErrorMessages(page: Page): Promise<void>;
+  triggerErrorMessages(page: Page, welsh: boolean): Promise<void>;
 };
 
 const subjectDetailsPage: SubjectDetailsPage = {
@@ -26,34 +26,69 @@ const subjectDetailsPage: SubjectDetailsPage = {
   continueButton: "#main-form-submit",
   rejectCookiesButton: ".cookie-banner-reject-button",
 
-  async checkPageLoads(page: Page, accessibilityTest: boolean) {
-    await expect(page.locator(".govuk-heading-l")).toHaveText(
-      subjectDetailsContent.pageTitle,
-    );
-    await expect(page.locator(".govuk-hint").nth(0)).toHaveText(
-      subjectDetailsContent.hintText1,
-    );
-    await expect(page.locator(".govuk-hint").nth(1)).toHaveText(
-      subjectDetailsContent.hintText2,
-    );
-    await expect(page.locator(".govuk-label").nth(0)).toHaveText(
-      subjectDetailsContent.subHeading1,
-    );
-    await expect(page.locator(".govuk-fieldset__legend")).toHaveText(
-      subjectDetailsContent.subHeading2,
-    );
-    await expect(page.locator("#subjectDateOfBirth-hint")).toHaveText(
-      subjectDetailsContent.hintText2,
-    );
-    await expect(page.locator(".govuk-label").nth(1)).toHaveText(
-      subjectDetailsContent.textOnPage1,
-    );
-    await expect(page.locator(".govuk-label").nth(2)).toHaveText(
-      subjectDetailsContent.textOnPage2,
-    );
-    await expect(page.locator(".govuk-label").nth(3)).toHaveText(
-      subjectDetailsContent.textOnPage3,
-    );
+  async checkPageLoads(page: Page, welsh: boolean, accessibilityTest: boolean) {
+    switch (welsh) {
+      case true:
+        await page.locator(".govuk-link.language").click();
+        await expect(page.locator(".govuk-link.language")).toHaveText("English");
+        await expect(page.locator(".govuk-heading-l")).toHaveText(
+          subjectDetailsContent.welshPageTitle,
+        );
+        await expect(page.locator(".govuk-hint").nth(0)).toHaveText(
+          subjectDetailsContent.welshHintText1,
+        );
+        await expect(page.locator(".govuk-hint").nth(1)).toHaveText(
+          subjectDetailsContent.welshHintText2,
+        );
+        await expect(page.locator(".govuk-label").nth(0)).toHaveText(
+          subjectDetailsContent.welshSubHeading1,
+        );
+        await expect(page.locator(".govuk-fieldset__legend")).toHaveText(
+          subjectDetailsContent.welshSubHeading2,
+        );
+        await expect(page.locator("#subjectDateOfBirth-hint")).toHaveText(
+          subjectDetailsContent.welshHintText2,
+        );
+        await expect(page.locator(".govuk-label").nth(1)).toHaveText(
+          subjectDetailsContent.welshTextOnPage1,
+        );
+        await expect(page.locator(".govuk-label").nth(2)).toHaveText(
+          subjectDetailsContent.welshTextOnPage2,
+        );
+        await expect(page.locator(".govuk-label").nth(3)).toHaveText(
+          subjectDetailsContent.welshTextOnPage3,
+        );
+        break;
+      case false:
+        await expect(page.locator(".govuk-heading-l")).toHaveText(
+          subjectDetailsContent.pageTitle,
+        );
+        await expect(page.locator(".govuk-hint").nth(0)).toHaveText(
+          subjectDetailsContent.hintText1,
+        );
+        await expect(page.locator(".govuk-hint").nth(1)).toHaveText(
+          subjectDetailsContent.hintText2,
+        );
+        await expect(page.locator(".govuk-label").nth(0)).toHaveText(
+          subjectDetailsContent.subHeading1,
+        );
+        await expect(page.locator(".govuk-fieldset__legend")).toHaveText(
+          subjectDetailsContent.subHeading2,
+        );
+        await expect(page.locator("#subjectDateOfBirth-hint")).toHaveText(
+          subjectDetailsContent.hintText2,
+        );
+        await expect(page.locator(".govuk-label").nth(1)).toHaveText(
+          subjectDetailsContent.textOnPage1,
+        );
+        await expect(page.locator(".govuk-label").nth(2)).toHaveText(
+          subjectDetailsContent.textOnPage2,
+        );
+        await expect(page.locator(".govuk-label").nth(3)).toHaveText(
+          subjectDetailsContent.textOnPage3,
+        );
+        break;
+    }
     if (accessibilityTest) {
       await axeTest(page);
     }
@@ -71,23 +106,46 @@ const subjectDetailsPage: SubjectDetailsPage = {
     await page.click(this.continueButton);
   },
 
-  async triggerErrorMessages(page: Page) {
-    await page.click(this.continueButton);
-    await expect(page.locator(".govuk-error-summary__title")).toHaveText(
-      subjectDetailsContent.errorBanner,
-    );
-    await expect(page.locator("[href='#subjectFullName']")).toHaveText(
-      subjectDetailsContent.fullNameError,
-    );
-    await expect(page.locator("[href='#subjectDateOfBirth']")).toHaveText(
-      subjectDetailsContent.dateOfBirthError,
-    );
-    await expect(page.locator("#subjectFullName-error")).toContainText(
-      subjectDetailsContent.fullNameError,
-    );
-    await expect(page.locator("#subjectDateOfBirth-error")).toContainText(
-      subjectDetailsContent.dateOfBirthError,
-    );
+  async triggerErrorMessages(page: Page, welsh: boolean) {
+    switch (welsh) {
+      case true:
+        await expect(page.locator(".govuk-link.language")).toHaveText("English");
+        await page.click(this.continueButton);
+        await expect(page.locator(".govuk-error-summary__title")).toHaveText(
+          subjectDetailsContent.welshErrorBanner,
+        );
+        await expect(page.locator("[href='#subjectFullName']")).toHaveText(
+          subjectDetailsContent.welshFullNameError,
+        );
+        await expect(page.locator("[href='#subjectDateOfBirth']")).toHaveText(
+          subjectDetailsContent.welshDateOfBirthError,
+        );
+        await expect(page.locator("#subjectFullName-error")).toContainText(
+          subjectDetailsContent.welshFullNameError,
+        );
+        await expect(page.locator("#subjectDateOfBirth-error")).toContainText(
+          subjectDetailsContent.welshDateOfBirthError,
+        );
+        break;
+      case false:
+        await page.click(this.continueButton);
+        await expect(page.locator(".govuk-error-summary__title")).toHaveText(
+          subjectDetailsContent.errorBanner,
+        );
+        await expect(page.locator("[href='#subjectFullName']")).toHaveText(
+          subjectDetailsContent.fullNameError,
+        );
+        await expect(page.locator("[href='#subjectDateOfBirth']")).toHaveText(
+          subjectDetailsContent.dateOfBirthError,
+        );
+        await expect(page.locator("#subjectFullName-error")).toContainText(
+          subjectDetailsContent.fullNameError,
+        );
+        await expect(page.locator("#subjectDateOfBirth-error")).toContainText(
+          subjectDetailsContent.dateOfBirthError,
+        );
+        break;
+    }
   },
 };
 
