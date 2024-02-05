373fbb632fd0dc3eea0cfe9db807f097b87d3cb9
JakeCG <jakecgowler@gmail.com>
2024-02-05 12:28:06 +0000

    Enabling welsh tests (#43)

--- src/tests/E2E/createCaseFE.test.ts
+++ src/tests/E2E/createCaseFE.test.ts
@@ -309,7 +309,7 @@ test("As a Citizen, Create an application with all details, a qualified represen
   );
 });
 
-test.skip("As a Citizen, Create an application with all details, a qualified representative, additional information, no PCQ, and submit - Cy", async ({
+test("As a Citizen, Create an application with all details, a qualified representative, additional information, no PCQ, and submit - Cy", async ({
   page,
 }) => {
   const cy = true,
@@ -357,7 +357,7 @@ test("Create an application with no representative, additional information, no P
   );
 });
 
-test.skip("Create an application with no representative, additional information, no PCQ, and submit - Cy", async ({
+test("Create an application with no representative, additional information, no PCQ, and submit - Cy", async ({
   page,
 }) => {
   const cy = true,
@@ -405,7 +405,7 @@ test("Create an application with all details, a qualified representative, no add
   );
 });
 
-test.skip("Create an application with all details, a qualified representative, no additional information, no PCQ, and submit - Cy.", async ({
+test("Create an application with all details, a qualified representative, no additional information, no PCQ, and submit - Cy.", async ({
   page,
 }) => {
   const cy = true,
@@ -453,7 +453,7 @@ test("Create an application with all details, an unqualified representative, no 
   );
 });
 
-test.skip("Create an application with all details, an unqualified representative, no additional information, no PCQ, and submit - Cy.", async ({
+test("Create an application with all details, an unqualified representative, no additional information, no PCQ, and submit - Cy.", async ({
   page,
 }) => {
   const cy = true,
@@ -521,7 +521,7 @@ test("Error messaging", async ({ page }) => {
   );
 });
 
-test.skip("Error messaging - Cy", async ({ page }) => {
+test("Error messaging - Cy", async ({ page }) => {
   const cy = true,
     representationPresent = true,
     representationQualified = true,
