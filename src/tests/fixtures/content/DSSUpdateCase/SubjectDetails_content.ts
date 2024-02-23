84709931e44c41c1fc14d610d23c995c1153c8af
jessieharrigan <144362161+jessieharrigan@users.noreply.github.com>
2024-02-23 13:01:10 +0000

    DTSSTCI-266 (#59)
    * Created tests for inputting subject details
    
    * Error messaging and back button journeys
    
    * Remove .only

---
+++ src/tests/fixtures/content/DSSUpdateCase/SubjectDetails_content.ts
@@ -0,0 +1,20 @@
+const subjectDetailsContent = {
+  header: "Update an appeal to the First-tier Tribunal",
+  pageTitle: "Subject of this case",
+  subHeading1: "Full name",
+  subHeading2: "Date of birth",
+  textOnPage1: "Day",
+  textOnPage2: "Month",
+  textOnPage3: "Year",
+  name: "Subject AutoTesting",
+  dayOfBirth: "1",
+  monthOfBirth: "1",
+  yearOfBirth: "2000",
+  errorBanner: "There is a problem on this page",
+  fullNameError: "Please enter full name",
+  dateOfBirthError: "Please enter date of birth",
+  validFullNameError: "Please enter a valid name",
+  validDateOfBirthError: "Please enter valid date of birth",
+} as const;
+
+export default subjectDetailsContent;
