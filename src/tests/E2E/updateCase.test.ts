9cce0d0002719e3c46462e835b14ae09c15a5f03
jessieharrigan <144362161+jessieharrigan@users.noreply.github.com>
2024-02-26 15:21:21 +0000

    Updated tests and content

--- src/tests/E2E/updateCase.test.ts
+++ src/tests/E2E/updateCase.test.ts
@@ -1,73 +1,133 @@
 import { test } from "@playwright/test";
-import { createFEApplication } from "../journeys/DSSCreateCase/createCase.ts";
+import createFEApplication from "../journeys/DSSCreateCase/createCase.ts";
 import updateCaseJourney from "../journeys/DSSUpdateCase/updateCase.ts";
 
 test.describe("DSS Update case tests. @DSSUpdate", () => {
-  test("Check for an existing case to update.", async ({ page }) => {
-    const caseNumber: string | void = await createFEApplication(
+  test("Check for an existing case to update and upload one document.", async ({
+    page,
+  }) => {
+    const caseNumber: string | void =
+      await createFEApplication.createFEApplication(
+        page,
+        false,
+        false,
+        false,
+        true,
+        false,
+        true,
+        false,
+        false,
+        false,
+      );
+    await updateCaseJourney.updateCase(
       page,
       false,
-      false,
-      false,
-      true,
-      false,
-      true,
+      caseNumber,
       false,
       false,
       false,
     );
-    await updateCaseJourney.updateCase(page, false, caseNumber, false, false);
+  });
+
+  test("Check for an existing case to update and upload multiple documents.", async ({
+    page,
+  }) => {
+    const caseNumber: string | void =
+      await createFEApplication.createFEApplication(
+        page,
+        false,
+        false,
+        false,
+        true,
+        false,
+        true,
+        false,
+        false,
+        false,
+      );
+    await updateCaseJourney.updateCase(
+      page,
+      false,
+      caseNumber,
+      true,
+      false,
+      false,
+    );
   });
 
   test("Test all back buttons on the Update Case application", async ({
     page,
   }) => {
-    const caseNumber: string | void = await createFEApplication(
+    const caseNumber: string | void =
+      await createFEApplication.createFEApplication(
+        page,
+        false,
+        false,
+        false,
+        true,
+        false,
+        true,
+        false,
+        true,
+        false,
+      );
+    await updateCaseJourney.updateCase(
       page,
       false,
-      false,
-      false,
-      true,
-      false,
-      true,
+      caseNumber,
       false,
       true,
       false,
     );
-    await updateCaseJourney.updateCase(page, true, caseNumber, true, false);
   });
 
   test("Error messaging", async ({ page }) => {
-    const caseNumber: string | void = await createFEApplication(
+    const caseNumber: string | void =
+      await createFEApplication.createFEApplication(
+        page,
+        false,
+        false,
+        false,
+        true,
+        false,
+        true,
+        false,
+        true,
+        false,
+      );
+    await updateCaseJourney.updateCase(
       page,
       false,
+      caseNumber,
       false,
       false,
       true,
-      false,
-      true,
-      false,
-      true,
-      false,
     );
-    await updateCaseJourney.updateCase(page, true, caseNumber, false, true);
   });
 });
 
 test("Check for an existing case to update - aXe test as it proceeds. @UpdateAccessibility", async ({
   page,
 }) => {
-  const caseNumber: string | void = await createFEApplication(
+  const caseNumber: string | void =
+    await createFEApplication.createFEApplication(
+      page,
+      false,
+      false,
+      false,
+      true,
+      false,
+      true,
+      false,
+      true,
+      false,
+    );
+  await updateCaseJourney.updateCase(
     page,
-    false,
-    false,
-    false,
     true,
+    caseNumber,
     false,
-    true,
     false,
-    true,
     false,
   );
-  await updateCaseJourney.updateCase(page, true, caseNumber, false, false);
 });
