fafdf08495f0a9235cd4351e6b890dcbb2eff154
jessieharrigan <144362161+jessieharrigan@users.noreply.github.com>
2024-02-27 12:13:56 +0000

    Updated content

--- src/tests/fixtures/content/DSSUpdateCase/UploadDocuments_content.ts
+++ src/tests/fixtures/content/DSSUpdateCase/UploadDocuments_content.ts
@@ -2,10 +2,10 @@ const uploadDocumentsContent = {
   header: "Update an appeal to the First-tier Tribunal",
   pageTitle: "Add information to a case",
   hintText:
-    "Please provide any additional relevant information that you would like to add to this case. Don't include personal financial information, such as your banking details.",
+    "Please provide any additional relevant information that you would like to add to this case. Don't include any personal financial information, such as your banking details.",
   subTitle1: "Additional documents",
   textOnPage1:
-    "You may want to submit other documents, files or evidence that provides the Tribunal with more information about your case.",
+    "You may want to submit other documents, files, or evidence that provides the Tribunal with more information about your case.",
   textOnPage2:
     "This will depend on the nature of your appeal, application or claim. Specific examples can be found in your completed tribunal form.",
   textOnPage3: "These may include:",
@@ -15,17 +15,17 @@ const uploadDocumentsContent = {
   textOnPage7: "related reports",
   textOnPage8:
     "For each piece of additional information or evidence you are submitting, please state why it is relevant to your case.",
-  dropdownLink: "File uploads requirements",
+  dropdownLink: "File upload requirements",
   textOnPage9:
     "File formats: MS Word, MS Excel, PDF, JPG, PNG, TXT, RTF, MP4, MP3",
   textOnPage10: "File size per document: up to 20 megabytes (MB)",
-  textOnPage11: "File size per multimedia file: 30 megabytes (MB)",
+  textOnPage11: "File size per multimedia file: up to 30 megabytes (MB)",
   textOnPage12: "Files cannot be password protected",
   textOnPage13:
-    "You can’t upload executable (.exe), zip or other archive files due to virus risks.",
+    "You can't upload executable (.exe), zip or other archive files due to virus risks.",
   textOnPage14: "Upload a file",
   textOnPage15:
-    "Please explain why you feel this document is relevant to your case",
+    "Please explain why you feel this document is relevant to your case.",
   textOnPage16:
     'Once you have selected the file click "Upload file" to upload it.',
   button: "Upload file",
@@ -40,8 +40,6 @@ const uploadDocumentsContent = {
     "You cannot continue without providing additional information or a document",
   fileTypeError:
     "This service only accepts files in the formats - Ms Word, MS Excel, PDF, JPG, PNG, TXT, RTF, MP4, MP3",
-  exceedsFileSizeError:
-    "File size exceeds the maximum permitted value. Please upload a file that is less than 20MB (documents) or less than 30MB (multimedia files)",
 };
 
 export default uploadDocumentsContent;
