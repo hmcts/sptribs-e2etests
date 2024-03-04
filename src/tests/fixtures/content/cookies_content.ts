c04fd0df26402a65c813c70347a10caf0423bcdf
jessieharrigan <144362161+jessieharrigan@users.noreply.github.com>
2024-03-04 15:59:21 +0000

    DTSSTCI-545 (#72)
    * Check for cookies banner
    
    * Prettier
    
    * Create cookie function in Common Helpers
    
    * Refactor cookie function
    
    * Change to cookie file name
    
    * Remove pause

---
+++ src/tests/fixtures/content/cookies_content.ts
@@ -0,0 +1,8 @@
+const cookiesContent = {
+  title: "Cookies on ",
+  textOnPage1: "We use some essential cookies to make this service work.",
+  textOnPage2:
+    "We’d also like to use analytics cookies so we can understand how you use the service and make improvements.",
+} as const;
+
+export default cookiesContent;
