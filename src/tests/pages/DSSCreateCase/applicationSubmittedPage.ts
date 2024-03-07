5278bf82e63afd227c54bf2d2b6384089965bbd3
Jake Gowler <jakecgowler@gmail.com>
2024-03-07 11:57:38 +0000

    applicationSubmittedPage.ts

--- src/tests/pages/DSSCreateCase/applicationSubmittedPage.ts
+++ src/tests/pages/DSSCreateCase/applicationSubmittedPage.ts
@@ -23,56 +23,64 @@ const applicationSubmittedPage: ApplicationSubmittedPage = {
   ): Promise<void> {
     switch (cy) {
       case true:
-        await expect(page.locator(".govuk-panel__title")).toHaveText(
-          applicationSubmittedContent.pageTitleCy,
-        );
-        await expect(
-          page.locator("div[class='govuk-panel__body'] strong"),
-        ).toContainText(applicationSubmittedContent.subTitleCy1);
-        await expect(page.locator(".govuk-body").nth(4)).toHaveText(
-          applicationSubmittedContent.textOnPageCy1,
-        );
-        await expect(page.locator(".govuk-body").nth(5)).toHaveText(
-          applicationSubmittedContent.textOnPageCy2,
-        );
-        await expect(
-          page.locator(".govuk-notification-banner__title"),
-        ).toHaveText(applicationSubmittedContent.subTitleCy2);
-        await expect(
-          page.locator(".govuk-notification-banner__content"),
-        ).toContainText(applicationSubmittedContent.textOnPageCy3);
-        await expect(
-          page.locator(".govuk-notification-banner__content"),
-        ).toContainText(applicationSubmittedContent.textOnPageCy4);
-        await expect(
-          page.locator(".govuk-notification-banner__content"),
-        ).toContainText(applicationSubmittedContent.textOnPageCy5);
+        await Promise.all([
+          expect(page.locator(".govuk-panel__title")).toHaveText(
+            applicationSubmittedContent.pageTitleCy,
+          ),
+          expect(
+            page.locator("div[class='govuk-panel__body'] strong"),
+          ).toContainText(applicationSubmittedContent.subTitleCy1),
+          ...Array.from({ length: 2 }, (_, index) => {
+            const textOnPage = (applicationSubmittedContent as any)[
+              `textOnPageCy${index + 1}`
+            ];
+            return expect(
+              page.locator(".govuk-body").nth(index + 4),
+            ).toHaveText(textOnPage);
+          }),
+          expect(page.locator(".govuk-notification-banner__title")).toHaveText(
+            applicationSubmittedContent.subTitleCy2,
+          ),
+          expect(
+            page.locator(".govuk-notification-banner__content"),
+          ).toContainText(applicationSubmittedContent.textOnPageCy3),
+          expect(
+            page.locator(".govuk-notification-banner__content"),
+          ).toContainText(applicationSubmittedContent.textOnPageCy4),
+          expect(
+            page.locator(".govuk-notification-banner__content"),
+          ).toContainText(applicationSubmittedContent.textOnPageCy5),
+        ]);
         break;
       default:
-        await expect(page.locator(".govuk-panel__title")).toHaveText(
-          applicationSubmittedContent.pageTitle,
-        );
-        await expect(
-          page.locator("div[class='govuk-panel__body'] strong"),
-        ).toContainText(applicationSubmittedContent.subTitle1);
-        await expect(page.locator(".govuk-body").nth(4)).toHaveText(
-          applicationSubmittedContent.textOnPage1,
-        );
-        await expect(page.locator(".govuk-body").nth(5)).toHaveText(
-          applicationSubmittedContent.textOnPage2,
-        );
-        await expect(
-          page.locator(".govuk-notification-banner__title"),
-        ).toHaveText(applicationSubmittedContent.subTitle2);
-        await expect(
-          page.locator(".govuk-notification-banner__content"),
-        ).toContainText(applicationSubmittedContent.textOnPage3);
-        await expect(
-          page.locator(".govuk-notification-banner__content"),
-        ).toContainText(applicationSubmittedContent.textOnPage4);
-        await expect(
-          page.locator(".govuk-notification-banner__content"),
-        ).toContainText(applicationSubmittedContent.textOnPage5);
+        await Promise.all([
+          expect(page.locator(".govuk-panel__title")).toHaveText(
+            applicationSubmittedContent.pageTitle,
+          ),
+          expect(
+            page.locator("div[class='govuk-panel__body'] strong"),
+          ).toContainText(applicationSubmittedContent.subTitle1),
+          ...Array.from({ length: 2 }, (_, index) => {
+            const textOnPage = (applicationSubmittedContent as any)[
+              `textOnPage${index + 1}`
+            ];
+            return expect(
+              page.locator(".govuk-body").nth(index + 4),
+            ).toHaveText(textOnPage);
+          }),
+          expect(page.locator(".govuk-notification-banner__title")).toHaveText(
+            applicationSubmittedContent.subTitle2,
+          ),
+          expect(
+            page.locator(".govuk-notification-banner__content"),
+          ).toContainText(applicationSubmittedContent.textOnPage3),
+          expect(
+            page.locator(".govuk-notification-banner__content"),
+          ).toContainText(applicationSubmittedContent.textOnPage4),
+          expect(
+            page.locator(".govuk-notification-banner__content"),
+          ).toContainText(applicationSubmittedContent.textOnPage5),
+        ]);
         break;
     }
     if (accessibilityTest) {
