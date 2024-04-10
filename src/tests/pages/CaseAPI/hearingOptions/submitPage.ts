1761fc48f2832c604657f155300c3c7df692833a
jessieharrigan <144362161+jessieharrigan@users.noreply.github.com>
2024-04-10 15:42:33 +0100

    DTSSTCI-316 (#69)
    * Hearing options - region data page
    
    * Prettier
    
    * First page
    
    * First page
    
    * First page
    
    * Second page
    
    * Prettier
    
    * Submit page
    
    * Prettier
    
    * Remove unused variable
    
    * Create 'ready to list' and accessibility tests
    
    * Test for no region and no venue
    
    * Tests for venue not listed
    
    * Changes to case region
    
    * Refactored journey structure
    
    * Remove .only
    
    * Change to test description
    
    * Edit to imports
    
    ---------
    
    Co-authored-by: JakeCG <jakecgowler@gmail.com>

---
+++ src/tests/pages/CaseAPI/hearingOptions/submitPage.ts
@@ -0,0 +1,154 @@
+import { expect, Page } from "@playwright/test";
+import axeTest from "../../../helpers/accessibilityTestHelper.ts";
+import commonHelpers, {
+  caseRegionCode,
+} from "../../../helpers/commonHelpers.ts";
+import hearingOptionsHearingDetailsContent from "../../../fixtures/content/CaseAPI/hearingOptions/hearingOptionsHearingDetails_content.ts";
+import submitContent from "../../../fixtures/content/CaseAPI/hearingOptions/submit_content.ts";
+
+type SubmitPage = {
+  saveAndContinue: string;
+  previous: string;
+  cancel: string;
+  checkPageLoads(
+    page: Page,
+    region: boolean,
+    venue: boolean,
+    accessibilityTest: boolean,
+  ): Promise<void>;
+  checkValidInfo(
+    page: Page,
+    region: boolean,
+    caseRegionCode: caseRegionCode | null,
+    venue: boolean,
+    venueNotListed: boolean,
+    hearingFormat: string,
+    shortNoticeHearing: boolean,
+  ): Promise<void>;
+  continueOn(page: Page): Promise<void>;
+};
+
+const submitPage: SubmitPage = {
+  saveAndContinue: '[type="submit"]',
+  previous: ".button-secondary",
+  cancel: ".cancel",
+
+  async checkPageLoads(
+    page: Page,
+    region: boolean,
+    venue: boolean,
+    accessibilityTest: boolean,
+  ): Promise<void> {
+    await Promise.all([
+      expect(page.locator(".govuk-heading-l")).toHaveText(
+        submitContent.pageTitle,
+      ),
+      expect(page.locator(".text-16").nth(0)).toHaveText(
+        submitContent.textOnPage1,
+      ),
+      ...Array.from({ length: 4 }, (_, index) => {
+        const textOnPage = (submitContent as any)[`textOnPage${index + 4}`];
+        return commonHelpers.checkVisibleAndPresent(
+          page.locator(
+            `th.case-field-label > span.text-16:text-is("${textOnPage}")`,
+          ),
+          1,
+        );
+      }),
+      page.locator(this.previous).isVisible(),
+      page.locator(this.saveAndContinue).isVisible(),
+      page.locator(this.cancel).isVisible(),
+    ]);
+    if (region) {
+      await commonHelpers.checkVisibleAndPresent(
+        page.locator(
+          `th.case-field-label > span.text-16:text-is("${submitContent.textOnPage2}")`,
+        ),
+        1,
+      );
+    }
+    if (venue) {
+      await commonHelpers.checkVisibleAndPresent(
+        page.locator(
+          `th.case-field-label > span.text-16:text-is("${submitContent.textOnPage3}")`,
+        ),
+        1,
+      );
+    }
+    if (accessibilityTest) {
+      await axeTest(page);
+    }
+  },
+
+  async checkValidInfo(
+    page: Page,
+    region: boolean,
+    caseRegionCode: caseRegionCode | null,
+    venue: boolean,
+    venueNotListed: boolean,
+    hearingFormat: string,
+    shortNoticeHearing: boolean,
+  ): Promise<void> {
+    if (region) {
+      await commonHelpers.checkVisibleAndPresent(
+        page.locator(
+          `ccd-read-dynamic-list-field > span.text-16:text-is("${caseRegionCode}")`,
+        ),
+        1,
+      );
+    }
+    if (venue) {
+      await commonHelpers.checkVisibleAndPresent(
+        page.locator(
+          `ccd-read-dynamic-list-field > span.text-16:text-is("${hearingOptionsHearingDetailsContent.venue}")`,
+        ),
+        1,
+      );
+    }
+    if (venueNotListed) {
+      await commonHelpers.checkVisibleAndPresent(
+        page.locator(
+          `span.text-16:text-is("${hearingOptionsHearingDetailsContent.textOnPage2}")`,
+        ),
+        1,
+      );
+    }
+    await Promise.all([
+      commonHelpers.checkVisibleAndPresent(
+        page.locator(
+          `ccd-read-text-field > span.text-16:text-is("${hearingOptionsHearingDetailsContent.room}")`,
+        ),
+        1,
+      ),
+      commonHelpers.checkVisibleAndPresent(
+        page.locator(
+          `ccd-read-text-field > span.text-16:text-is("${hearingOptionsHearingDetailsContent.instructions}")`,
+        ),
+        1,
+      ),
+      commonHelpers.checkVisibleAndPresent(
+        page.locator(
+          `ccd-read-fixed-radio-list-field > span.text-16:text-is("${hearingFormat}")`,
+        ),
+        1,
+      ),
+    ]);
+    if (shortNoticeHearing) {
+      await commonHelpers.checkVisibleAndPresent(
+        page.locator(`ccd-read-yes-no-field > span.text-16:text-is("Yes")`),
+        1,
+      );
+    } else {
+      await commonHelpers.checkVisibleAndPresent(
+        page.locator(`ccd-read-yes-no-field > span.text-16:text-is("No")`),
+        1,
+      );
+    }
+  },
+
+  async continueOn(page: Page): Promise<void> {
+    await page.click(this.saveAndContinue);
+  },
+};
+
+export default submitPage;
