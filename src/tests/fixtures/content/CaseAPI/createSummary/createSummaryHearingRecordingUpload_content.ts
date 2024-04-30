afa0599058e6edc1b699ba1c1b7843d5ae7937e1
jessieharrigan <144362161+jessieharrigan@users.noreply.github.com>
2024-04-30 16:50:43 +0100

    Content pages

---
+++ src/tests/fixtures/content/CaseAPI/createSummary/createSummaryHearingRecordingUpload_content.ts
@@ -0,0 +1,19 @@
+const createSummaryHearingRecordingUpload_content = {
+  pageHint: "Hearings:Create summary",
+  pageTitle: "Upload hearing recording",
+  caseReference: "Case number: ",
+  title: "Upload the recording of the hearing (Optional)",
+  textOnPage1: "Advice on uploads",
+  textOnPage2: "File must be no larger than 500 MB",
+  textOnPage3: "You can only upload mp3 files",
+  textOnPage4: "Give the files a meaningful name. for example, bail-hearing-John-Smith.mp3",
+  textOnPage5: "Note: If the remove button is disabled, please refresh the page to remove attachments",
+  subTitle1: "Upload file (Optional)",
+  textOnPage6: "Document Category (Optional)",
+  textOnPage7: "Description (Optional)",
+  textOnPage8: "File (Optional)",
+  textOnPage9: "If you can't upload a recording of the hearing, please describe where it can be found. You can also enter a link to the recording",
+  textOnPage10: "Where can the recording be found? (Optional)",
+}
+
+export default createSummaryHearingRecordingUpload_content;
