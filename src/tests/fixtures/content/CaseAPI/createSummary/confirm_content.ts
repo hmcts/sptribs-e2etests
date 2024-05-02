ccce89172d4be1dd9a76ae88e0b48af7687699fa
jessieharrigan <144362161+jessieharrigan@users.noreply.github.com>
2024-05-02 09:25:09 +0100

    Confirm page

--- src/tests/fixtures/content/CaseAPI/createSummary/confirm_content.ts
+++ src/tests/fixtures/content/CaseAPI/createSummary/confirm_content.ts
@@ -2,8 +2,7 @@ const confirm_content = {
   pageHint: "Hearings:Create summary",
   caseReference: "Case number: ",
   pageTitle: "Hearing summary created",
-  textOnPage:
-    "This hearing summary has been added to the case record.",
+  textOnPage: "This hearing summary has been added to the case record.",
 } as const;
 
 export default confirm_content;
