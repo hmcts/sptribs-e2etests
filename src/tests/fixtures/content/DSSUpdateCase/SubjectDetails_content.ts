ff5e111c597d1cb7729c6e2c74543707c55cf6f8
Jake Gowler <jakecgowler@gmail.com>
2024-05-07 14:18:31 +0100

    DTSSTCI-727 - rewording error messaging.

--- src/tests/fixtures/content/DSSUpdateCase/SubjectDetails_content.ts
+++ src/tests/fixtures/content/DSSUpdateCase/SubjectDetails_content.ts
@@ -13,10 +13,13 @@ const subjectDetailsContent = {
   monthOfBirth: "1",
   yearOfBirth: "2000",
   errorBanner: "There is a problem",
-  fullNameError: "Please enter a full name. This name must match exactly with the case record",
+  fullNameError:
+    "Please enter a full name. This name must match exactly with the case record",
   dateOfBirthError: "Please enter a full date of birth. For example, 31 3 1980",
-  validFullNameError: "Please enter a valid name. This name must match exactly with the case record",
-  validDateOfBirthError: "Please enter a valid date of birth. For example, 31 3 1980",
+  validFullNameError:
+    "Please enter a valid name. This name must match exactly with the case record",
+  validDateOfBirthError:
+    "Please enter a valid date of birth. For example, 31 3 1980",
 
   headerCy: "Diweddaru apêl i’r Tribiwnlys Haen Gyntaf",
   pageTitleCy: "Testun yr achos hwn",
@@ -28,10 +31,13 @@ const subjectDetailsContent = {
   textOnPageCy2: "Mis",
   textOnPageCy3: "Blwyddyn",
   errorBannerCy: "Mae yna broblem",
-  fullNameErrorCy: "Nodwch enw llawn. Rhaid i'r enw hwn gyd-fynd yn union â chofnod yr achos",
+  fullNameErrorCy:
+    "Nodwch enw llawn. Rhaid i'r enw hwn gyd-fynd yn union â chofnod yr achos",
   dateOfBirthErrorCy: "Nodwch ddyddiad geni. Er enghraifft, 31 3 1980",
-  validFullNameErrorCy: "Rhowch enw dilys. Rhaid i'r enw hwn gyd-fynd yn union â chofnod yr achos",
-  validDateOfBirthErrorCy: "Nodwch ddyddiad geni dilys. Er enghraifft, 31 3 1980",
+  validFullNameErrorCy:
+    "Rhowch enw dilys. Rhaid i'r enw hwn gyd-fynd yn union â chofnod yr achos",
+  validDateOfBirthErrorCy:
+    "Nodwch ddyddiad geni dilys. Er enghraifft, 31 3 1980",
 } as const;
 
 export default subjectDetailsContent;
