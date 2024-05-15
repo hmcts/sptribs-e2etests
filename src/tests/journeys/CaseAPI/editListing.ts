8d082ffac99b4cbe28d0855e633e2ca71e764668
jessieharrigan <144362161+jessieharrigan@users.noreply.github.com>
2024-05-15 12:06:49 +0100

    Second page

--- src/tests/journeys/CaseAPI/editListing.ts
+++ src/tests/journeys/CaseAPI/editListing.ts
@@ -9,6 +9,7 @@ import commonHelpers, {
   hearingVenues,
 } from "../../helpers/commonHelpers.ts";
 import editListingSelectHearingPage from "../../pages/CaseAPI/editListing/editListingSelectHearingPage.ts";
+import editListingHearingTypeAndFormatPage from "../../pages/CaseAPI/editListing/editListingHearingTypeAndFormatPage.ts";
 
 type EditListing = {
   editListing(
@@ -55,29 +56,50 @@ const editListing: EditListing = {
       "Birmingham Civil And Family Justice Centre-Priory Courts, 33 Bull Street",
       false,
     );
-    await commonHelpers.chooseEventFromDropdown(
-      page,
-      "Hearings: Edit listing",
-    );
+    await commonHelpers.chooseEventFromDropdown(page, "Hearings: Edit listing");
     if (caseNumber !== undefined) {
       switch (errorMessaging) {
         default:
-          await editListingSelectHearingPage.checkPageLoads(page, caseNumber, accessibilityTest);
+          await editListingSelectHearingPage.checkPageLoads(
+            page,
+            caseNumber,
+            accessibilityTest,
+          );
           const hearing = await editListingSelectHearingPage.fillInFields(page);
           await editListingSelectHearingPage.continueOn(page);
-
+          await editListingHearingTypeAndFormatPage.checkPageLoads(
+            page,
+            caseNumber,
+            accessibilityTest,
+          );
+          await editListingHearingTypeAndFormatPage.fillInFields(
+            page,
+            hearingType,
+            hearingFormat,
+          );
+          await editListingHearingTypeAndFormatPage.continueOn(page);
 
           break;
         case true:
-          await editListingSelectHearingPage.checkPageLoads(page, caseNumber, accessibilityTest);
+          await editListingSelectHearingPage.checkPageLoads(
+            page,
+            caseNumber,
+            accessibilityTest,
+          );
           await editListingSelectHearingPage.triggerErrorMessages(page);
           await editListingSelectHearingPage.fillInFields(page);
           await editListingSelectHearingPage.continueOn(page);
+          await editListingHearingTypeAndFormatPage.checkPageLoads(
+            page,
+            caseNumber,
+            accessibilityTest,
+          );
+          await editListingHearingTypeAndFormatPage.continueOn(page);
 
           break;
       }
     } else {
-      throw new Error("Case number is undefined.")
+      throw new Error("Case number is undefined.");
     }
   },
 };
