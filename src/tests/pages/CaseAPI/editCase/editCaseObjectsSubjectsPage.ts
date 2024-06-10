cbbec24c774b73281a02b192e9ebbbe8cfc97614
jessieharrigan <144362161+jessieharrigan@users.noreply.github.com>
2024-06-10 17:33:51 +0100

    Third page

---
+++ src/tests/pages/CaseAPI/editCase/editCaseObjectsSubjectsPage.ts
@@ -0,0 +1,126 @@
+import { expect, Page } from "@playwright/test";
+import axeTest from "../../../helpers/accessibilityTestHelper.ts";
+import { SubCategory } from "../../../helpers/commonHelpers.ts";
+import { initialState } from "../../../journeys/CaseAPI/editCase.ts";
+import editCaseObjectsSubjectsContent from "../../../fixtures/content/CaseAPI/editCase/editCaseObjectsSubjects_content.ts";
+import caseSubjectDetailsObject_content from "../../../fixtures/content/CaseAPI/createCase/caseSubjectDetailsObject_content.ts";
+
+type EditCaseObjectsSubjectsPage = {
+  continue: string;
+  subjectSelectBox: string;
+  representativeSelectBox: string;
+  applicantSelectBox: string;
+  checkPageLoads(
+    page: Page,
+    caseNumber: string,
+    accessibilityTest: boolean,
+  ): Promise<void>;
+  checkAndFillInFields(
+    page: Page,
+    initialState: initialState,
+    representative: boolean,
+    applicant: boolean,
+    subcategory: SubCategory,
+  ): Promise<void>;
+  triggerErrorMessages(page: Page): Promise<void>;
+};
+
+const editCaseObjectsSubjectsPage: EditCaseObjectsSubjectsPage = {
+  continue: '[type="submit"]',
+  subjectSelectBox: "#cicCasePartiesCIC-SubjectCIC",
+  representativeSelectBox: "#cicCasePartiesCIC-RepresentativeCIC",
+  applicantSelectBox: "#cicCasePartiesCIC-ApplicantCIC",
+
+  async checkPageLoads(
+    page: Page,
+    caseNumber: string,
+    accessibilityTest: boolean,
+  ): Promise<void> {
+    await Promise.all([
+      expect(page.locator(".govuk-caption-l")).toHaveText(
+        editCaseObjectsSubjectsContent.pageHint,
+      ),
+      expect(page.locator(".govuk-heading-l")).toHaveText(
+        editCaseObjectsSubjectsContent.pageTitle,
+      ),
+      expect(page.locator("markdown > h3")).toContainText(
+        caseSubjectDetailsObject_content.name,
+      ),
+      expect(page.locator("markdown > p").nth(0)).toContainText(
+        editCaseObjectsSubjectsContent.caseReference + caseNumber,
+      ),
+      ...Array.from({ length: 4 }, (_, index) => {
+        const textOnPage = (editCaseObjectsSubjectsContent as any)[
+          `textOnPage${index + 1}`
+        ];
+        return expect(page.locator(".form-label").nth(index)).toHaveText(
+          textOnPage,
+        );
+      }),
+    ]);
+    if (accessibilityTest) {
+      await axeTest(page);
+    }
+  },
+
+  async checkAndFillInFields(
+    page: Page,
+    initialState: initialState,
+    representative: boolean,
+    applicant: boolean,
+    subCategory: SubCategory,
+  ): Promise<void> {
+    if (
+      (!applicant && subCategory === "Minor") ||
+      (!applicant && subCategory === "Fatal")
+    ) {
+      throw new Error("Cannot have a Minor or Fatal case with no applicant.");
+    }
+    await expect(page.locator(this.subjectSelectBox)).toBeChecked();
+    if (initialState !== "DSS Submitted") {
+      await expect(page.locator(this.representativeSelectBox)).toBeChecked();
+      await expect(page.locator(this.applicantSelectBox)).toBeChecked();
+      if (!representative) {
+        await page.click(this.representativeSelectBox);
+      }
+      if (!applicant) {
+        await page.click(this.applicantSelectBox);
+      }
+      await page.click(this.continue);
+    } else {
+      if (representative) {
+        await page.click(this.representativeSelectBox);
+      }
+      if (applicant) {
+        await page.click(this.applicantSelectBox);
+      }
+    }
+  },
+
+  async triggerErrorMessages(page: Page): Promise<void> {
+    await page.click(this.subjectSelectBox);
+    await page.click(this.continue);
+    await Promise.all([
+      expect(page.locator("#error-summary-title")).toHaveText(
+        editCaseObjectsSubjectsContent.errorBanner,
+      ),
+      expect(page.locator(".validation-error")).toHaveText(
+        editCaseObjectsSubjectsContent.partiesError,
+      ),
+      expect(page.locator(".error-message")).toHaveText(
+        editCaseObjectsSubjectsContent.partiesError,
+      ),
+    ]);
+    await page.click(this.representativeSelectBox);
+    await page.click(this.continue);
+    await Promise.all([
+      expect(page.locator(".error-summary-list")).toContainText(
+        editCaseObjectsSubjectsContent.subjectError,
+      ),
+    ]);
+    await page.click(this.representativeSelectBox);
+    await page.click(this.subjectSelectBox);
+  },
+};
+
+export default editCaseObjectsSubjectsPage;
