b09267038276f4bc5ed5b9af944f24dc2450c69f
Jake Gowler <jakecgowler@gmail.com>
2024-08-08 11:19:10 +0100

    Updating E2E's to check for new fields

--- src/tests/journeys/DSSCreateCase/createCase.ts
+++ src/tests/journeys/DSSCreateCase/createCase.ts
@@ -215,6 +215,8 @@ const createFEApplication: CreateFeApplication = {
       page,
       cy,
       representationPresent,
+      uploadOtherInfo,
+      multipleDocuments,
       accessibilityTest,
     );
     await checkYourAnswersPage.checkValidInfoAllFields(
