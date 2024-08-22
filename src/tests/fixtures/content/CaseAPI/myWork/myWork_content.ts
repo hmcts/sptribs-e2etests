136477124d8eb626567e2af5a85836f7dfb30b8f
jessieharrigan <144362161+jessieharrigan@users.noreply.github.com>
2024-08-22 13:10:01 +0100

    Start of register new case journey

---
+++ src/tests/fixtures/content/CaseAPI/myWork/myWork_content.ts
@@ -0,0 +1,24 @@
+const myWork_content = {
+  pageTitle: "My work",
+  hintText:
+    "Use the work filter to show tasks and cases based on service, work type and location",
+  filterSubtitle1: "Services",
+  filterSubtitle2: "Search for a location by name",
+  filterHintText: "Leave blank to return all locations available to you.",
+  filterSubtitle3: "Types of work",
+  filterButton1: "Add location",
+  filterButton2: "Apply",
+  tab1: "My tasks",
+  tab2: "Available tasks",
+  tab3: "My cases",
+  tab4: "My access",
+  column1: "Case name",
+  column2: "Case category",
+  column3: "Location",
+  column4: "Task",
+  column5: "Due date",
+  column6: "Hearing date",
+  column7: "Priority",
+} as const;
+
+export default myWork_content;
---
+++ src/tests/journeys/WA/registerNewCase.ts
@@ -0,0 +1,56 @@
+import { Page } from "@playwright/test";
+import { UserRole } from "../../config.ts";
+import createFEApplication from "../DSSCreateCase/createCase.ts";
+import caseAPILoginPage from "../../pages/CaseAPI/caseList/caseAPILoginPage.ts";
+import myWorkPage from "../../pages/CaseAPI/myWork/myWorkPage.ts";
+
+type RegisterNewCase = {
+  seeTask(
+    page: Page,
+    user: UserRole,
+    accessibilityTest: boolean,
+  ): Promise<void>;
+  completeTask(
+    page: Page,
+    user: UserRole,
+    accessibilityTest: boolean,
+  ): Promise<void>;
+};
+
+const registerNewCase: RegisterNewCase = {
+  async seeTask(
+    page: Page,
+    user: UserRole,
+    accessibilityTest: boolean,
+  ): Promise<void> {
+    let caseNumber: string | void;
+    caseNumber = await createFEApplication.createFEApplication(
+      page,
+      false,
+      "demoCitizen",
+      true,
+      true,
+      false,
+      false,
+      true,
+      false,
+      false,
+      false,
+    );
+    await page.locator(`a:text-is(" Sign out ")`).click();
+    await page.waitForLoadState("domcontentloaded");
+    await caseAPILoginPage.SignInUser(page, user);
+    await page.pause();
+    await myWorkPage.checkPageLoads(page, accessibilityTest);
+  },
+
+  async completeTask(
+    page: Page,
+    user: UserRole,
+    accessibilityTest: boolean,
+  ): Promise<void> {
+    await this.seeTask(page, user, accessibilityTest);
+  },
+};
+
+export default registerNewCase;
