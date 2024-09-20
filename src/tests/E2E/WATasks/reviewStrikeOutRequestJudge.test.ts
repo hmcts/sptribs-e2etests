49a7d42028d0e854fde16ddcf32819d07d85b66e
jessieharrigan <144362161+jessieharrigan@users.noreply.github.com>
2024-09-20 11:20:16 +0100

    Remove .only

--- src/tests/E2E/WATasks/reviewStrikeOutRequestJudge.test.ts
+++ src/tests/E2E/WATasks/reviewStrikeOutRequestJudge.test.ts
@@ -21,7 +21,7 @@ const eventOrders = "Orders: Create draft";
 const stateBeforeCompletion = "Case Status:  Case management";
 const stateAfterCompletion = "Case Status:  Case management";
 
-test.describe.only("Review Strike Out Request - Judge @CaseAPI", (): void => {
+test.describe("Review Strike Out Request - Judge @CaseAPI", (): void => {
   test("Task is completable via next steps link - assign to me and go to task - CIC8 - ME Joint Instruction", async ({
     page,
   }) => {
