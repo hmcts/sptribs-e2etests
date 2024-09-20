e4179486f1f449c813d259873aec98ce49d6890c
jessieharrigan <144362161+jessieharrigan@users.noreply.github.com>
2024-09-20 14:58:05 +0100

    Prettier

--- src/removedFiles/createDraft.test.ts
+++ src/removedFiles/createDraft.test.ts
@@ -2,44 +2,44 @@
 // import createDraft from "./createDraft.ts";
 
 // test.describe("Case-API Create draft tests. @CaseAPI", () => {
-  // test("As a Caseworker create a CIC3 draft in the Case Management state. @crossbrowserCaseAPI", async ({
-  //   page,
-  // }) => {
-  //   await createDraft.createDraft(
-  //     page,
-  //     "caseWorker",
-  //     "Case Management",
-  //     false,
-  //     false,
-  //     "CIC3 - Rule 27",
-  //   );
-  // });
+// test("As a Caseworker create a CIC3 draft in the Case Management state. @crossbrowserCaseAPI", async ({
+//   page,
+// }) => {
+//   await createDraft.createDraft(
+//     page,
+//     "caseWorker",
+//     "Case Management",
+//     false,
+//     false,
+//     "CIC3 - Rule 27",
+//   );
+// });
 
-  // test("As a Senior Caseworker create a CIC6 draft in the Ready to list state. @crossbrowserCaseAPI", async ({
-  //   page,
-  // }) => {
-  //   await createDraft.createDraft(
-  //     page,
-  //     "seniorCaseworker",
-  //     "Ready to list",
-  //     false,
-  //     false,
-  //     "CIC6 - General Directions",
-  //   );
-  // });
+// test("As a Senior Caseworker create a CIC6 draft in the Ready to list state. @crossbrowserCaseAPI", async ({
+//   page,
+// }) => {
+//   await createDraft.createDraft(
+//     page,
+//     "seniorCaseworker",
+//     "Ready to list",
+//     false,
+//     false,
+//     "CIC6 - General Directions",
+//   );
+// });
 
-  // test("As a hearing centre admin create a CIC7 draft in the Awaiting Hearing state.", async ({
-  //   page,
-  // }) => {
-  //   await createDraft.createDraft(
-  //     page,
-  //     "hearingCentreAdmin",
-  //     "Awaiting Hearing",
-  //     false,
-  //     false,
-  //     "CIC7 - ME Dmi Reports",
-  //   );
-  // });
+// test("As a hearing centre admin create a CIC7 draft in the Awaiting Hearing state.", async ({
+//   page,
+// }) => {
+//   await createDraft.createDraft(
+//     page,
+//     "hearingCentreAdmin",
+//     "Awaiting Hearing",
+//     false,
+//     false,
+//     "CIC7 - ME Dmi Reports",
+//   );
+// });
 
 //   test("As a hearing Centre Team Lead create a CIC8 draft in the Case Stayed state.", async ({
 //     page,
