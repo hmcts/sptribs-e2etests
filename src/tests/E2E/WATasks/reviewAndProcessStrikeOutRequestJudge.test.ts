b41c50d4bbdc07498ad48cca0e3decd4d9dfbd4c
jessieharrigan <144362161+jessieharrigan@users.noreply.github.com>
2024-09-30 15:19:10 +0100

    Process strike out directions returned - judge

--- src/tests/E2E/WATasks/reviewAndProcessStrikeOutRequestJudge.test.ts
+++ src/tests/E2E/WATasks/reviewAndProcessStrikeOutRequestJudge.test.ts
@@ -9,26 +9,34 @@ import events_content from "../../fixtures/content/CaseAPI/events_content.ts";
 import closeCase from "../../journeys/WA/closeCase.ts";
 import myWorkPage from "../../pages/WA/myWorkPage.ts";
 import referCaseToJudge from "../../journeys/WA/referCaseToJudge.ts";
+import sendOrder from "../../journeys/WA/sendOrder.ts";
 
 const taskName = "Review Strike out request - Judge";
-const priority = null;
-const assignedUser = "Ms Kayla Adams";
-const userRoleCreate = "waRegionalHearingCentreAdmin";
-const userRole = "waPrincipalJudge";
-const numberOfDays = 5;
+const taskNameProcess = "";
+const priorityReview = null;
+const priorityProcess = " low ";
+const assignedUserAdmin = "sptribswa regionalhearingcentreadmin";
+const assignedUserJudge = "Ms Kayla Adams";
+const userRoleAdmin = "waRegionalHearingCentreAdmin";
+const userRoleJudge = "waPrincipalJudge";
+const numberOfDaysReview = 5;
+// const numberOfDaysProcess = 7;
+// Awaiting bug fix
+const numberOfDaysProcess = 10;
 const eventRefer = "Refer case to judge";
 const eventOrders = "Orders: Create draft";
+const eventSendOrder = "Orders: Send order";
 const stateBeforeCompletion = "Case Status:  Case management";
 const stateAfterCompletion = "Case Status:  Case management";
 
-test.describe("Review Strike Out Request - Judge @CaseAPI", (): void => {
+test.describe.only("Review Strike Out Request - Judge @CaseAPI", (): void => {
   test("Task is completable via next steps link - assign to me and go to task - CIC8 - ME Joint Instruction", async ({
     page,
   }) => {
     let caseNumber01: any;
     caseNumber01 = await createCase.createCase(
       page,
-      userRoleCreate,
+      userRoleAdmin,
       false,
       "Assessment",
       "Other",
@@ -57,17 +65,17 @@ test.describe("Review Strike Out Request - Judge @CaseAPI", (): void => {
       false,
       caseNumber01,
     );
-    await task.seeTask(page, userRole, false, taskName);
+    await task.seeTask(page, userRoleJudge, false, taskName);
     await task.initiateTask(
       page,
-      userRole,
+      userRoleJudge,
       "Link: Assign Task to Me and Go To Task",
       false,
       caseNumber01,
       taskName,
-      priority,
-      assignedUser,
-      numberOfDays,
+      priorityReview,
+      assignedUserJudge,
+      numberOfDaysReview,
       eventOrders,
       stateBeforeCompletion,
     );
@@ -85,6 +93,37 @@ test.describe("Review Strike Out Request - Judge @CaseAPI", (): void => {
       caseNumber01,
       stateAfterCompletion,
     );
+    await task.seeTask(page, userRoleAdmin, false, taskNameProcess);
+    await task.initiateTask(
+      page,
+      userRoleAdmin,
+      "Link: Assign Task to Me and Go To Task",
+      false,
+      caseNumber01,
+      taskNameProcess,
+      priorityProcess,
+      assignedUserAdmin,
+      numberOfDaysProcess,
+      eventSendOrder,
+      stateBeforeCompletion,
+    );
+    await sendOrder.sendOrder(
+      page,
+      caseNumber01,
+      "DraftOrder",
+      false,
+      false,
+      true,
+      true,
+      "1",
+    );
+    await task.checkCompletedTask(
+      page,
+      false,
+      taskNameProcess,
+      caseNumber01,
+      stateAfterCompletion,
+    );
   });
 
   test("Task is completable via next steps link - assign to me - CIC10 - Strike Out Warning", async ({
@@ -93,7 +132,7 @@ test.describe("Review Strike Out Request - Judge @CaseAPI", (): void => {
     let caseNumber02: any;
     caseNumber02 = await createCase.createCase(
       page,
-      userRoleCreate,
+      userRoleAdmin,
       false,
       "Assessment",
       "Other",
@@ -124,21 +163,21 @@ test.describe("Review Strike Out Request - Judge @CaseAPI", (): void => {
     );
     await commonHelpers.signOutAndGoToCase(
       page,
-      userRole,
+      userRoleJudge,
       config.CaseAPIBaseURL,
       caseNumber02,
     );
-    await task.seeTask(page, userRole, false, taskName);
+    await task.seeTask(page, userRoleJudge, false, taskName);
     await task.initiateTask(
       page,
-      userRole,
+      userRoleJudge,
       "Link: Assign Task to Me",
       false,
       caseNumber02,
       taskName,
-      priority,
-      assignedUser,
-      numberOfDays,
+      priorityReview,
+      assignedUserJudge,
+      numberOfDaysReview,
       eventOrders,
       stateBeforeCompletion,
     );
@@ -156,6 +195,37 @@ test.describe("Review Strike Out Request - Judge @CaseAPI", (): void => {
       caseNumber02,
       stateAfterCompletion,
     );
+    await task.seeTask(page, userRoleAdmin, false, taskNameProcess);
+    await task.initiateTask(
+      page,
+      userRoleAdmin,
+      "Link: Assign Task to Me",
+      false,
+      caseNumber02,
+      taskNameProcess,
+      priorityProcess,
+      assignedUserAdmin,
+      numberOfDaysProcess,
+      eventSendOrder,
+      stateBeforeCompletion,
+    );
+    await sendOrder.sendOrder(
+      page,
+      caseNumber02,
+      "UploadOrder",
+      false,
+      false,
+      false,
+      true,
+      "3",
+    );
+    await task.checkCompletedTask(
+      page,
+      false,
+      taskNameProcess,
+      caseNumber02,
+      stateAfterCompletion,
+    );
   });
 
   test("Task is completed via event dropdown - CIC13 - Pro Forma Summons", async ({
@@ -164,7 +234,7 @@ test.describe("Review Strike Out Request - Judge @CaseAPI", (): void => {
     let caseNumber03: any;
     caseNumber03 = await createCase.createCase(
       page,
-      userRoleCreate,
+      userRoleAdmin,
       false,
       "Assessment",
       "Other",
@@ -195,21 +265,21 @@ test.describe("Review Strike Out Request - Judge @CaseAPI", (): void => {
     );
     await commonHelpers.signOutAndGoToCase(
       page,
-      userRole,
+      userRoleJudge,
       config.CaseAPIBaseURL,
       caseNumber03,
     );
-    await task.seeTask(page, userRole, false, taskName);
+    await task.seeTask(page, userRoleJudge, false, taskName);
     await task.initiateTask(
       page,
-      userRole,
+      userRoleJudge,
       "Event DropDown",
       false,
       caseNumber03,
       taskName,
-      priority,
-      assignedUser,
-      numberOfDays,
+      priorityReview,
+      assignedUserJudge,
+      numberOfDaysReview,
       eventOrders,
       stateBeforeCompletion,
     );
@@ -227,13 +297,44 @@ test.describe("Review Strike Out Request - Judge @CaseAPI", (): void => {
       caseNumber03,
       stateAfterCompletion,
     );
+    await task.seeTask(page, userRoleAdmin, false, taskNameProcess);
+    await task.initiateTask(
+      page,
+      userRoleAdmin,
+      "Event DropDown",
+      false,
+      caseNumber03,
+      taskNameProcess,
+      priorityProcess,
+      assignedUserAdmin,
+      numberOfDaysProcess,
+      eventSendOrder,
+      stateBeforeCompletion,
+    );
+    await sendOrder.sendOrder(
+      page,
+      caseNumber03,
+      "DraftOrder",
+      false,
+      false,
+      true,
+      true,
+      "5",
+    );
+    await task.checkCompletedTask(
+      page,
+      false,
+      taskNameProcess,
+      caseNumber03,
+      stateAfterCompletion,
+    );
   });
 
-  test("Task is cancellable through close case", async ({ page }) => {
+  test("Review task is cancellable through close case", async ({ page }) => {
     let caseNumber04: any;
     caseNumber04 = await createCase.createCase(
       page,
-      userRoleCreate,
+      userRoleAdmin,
       false,
       "Assessment",
       "Other",
@@ -264,11 +365,11 @@ test.describe("Review Strike Out Request - Judge @CaseAPI", (): void => {
     );
     await commonHelpers.signOutAndGoToCase(
       page,
-      userRole,
+      userRoleJudge,
       config.CaseAPIBaseURL,
       caseNumber04,
     );
-    await task.seeTask(page, userRole, false, taskName);
+    await task.seeTask(page, userRoleJudge, false, taskName);
     await myWorkPage.clickAssignAndGoToTask(page);
     await commonHelpers.chooseEventFromDropdown(page, events_content.closeCase);
     await closeCase.closeCase(
@@ -286,6 +387,89 @@ test.describe("Review Strike Out Request - Judge @CaseAPI", (): void => {
       false,
       taskName,
       caseNumber04,
+      "Case Status:  Case closed",
+    );
+  });
+
+  test("Process task is cancellable through close case", async ({ page }) => {
+    let caseNumber05: any;
+    caseNumber05 = await createCase.createCase(
+      page,
+      userRoleAdmin,
+      false,
+      "Assessment",
+      "Other",
+      true,
+      true,
+      "Email",
+      true,
+      false,
+      "1996",
+      "Scotland",
+      true,
+      true,
+      true,
+      false,
+      true,
+      false,
+    );
+    console.log(`Case Number : ${caseNumber05}`);
+    await commonHelpers.chooseEventFromDropdown(page, events_content.buildCase);
+    await buildCase.buildCase(page, false, caseNumber05);
+    await commonHelpers.chooseEventFromDropdown(page, eventRefer);
+    await referCaseToJudge.referCaseToJudge(
+      page,
+      false,
+      "Strike out request",
+      false,
+      caseNumber05,
+    );
+    await task.seeTask(page, userRoleJudge, false, taskName);
+    await task.initiateTask(
+      page,
+      userRoleJudge,
+      "Link: Assign Task to Me and Go To Task",
+      false,
+      caseNumber05,
+      taskName,
+      priorityReview,
+      assignedUserJudge,
+      numberOfDaysReview,
+      eventOrders,
+      stateBeforeCompletion,
+    );
+    await createDraft.createDraft(
+      page,
+      false,
+      false,
+      "CIC3 - Rule 27",
+      caseNumber05,
+    );
+    await task.checkCompletedTask(
+      page,
+      false,
+      taskName,
+      caseNumber05,
+      stateAfterCompletion,
+    );
+    await task.seeTask(page, userRoleAdmin, false, taskNameProcess);
+    await myWorkPage.clickAssignAndGoToTask(page);
+    await commonHelpers.chooseEventFromDropdown(page, events_content.closeCase);
+    await closeCase.closeCase(
+      page,
+      false,
+      false,
+      "caseRejected",
+      false,
+      "createdInError",
+      null,
+      caseNumber05,
+    );
+    await task.checkCompletedTask(
+      page,
+      false,
+      taskName,
+      caseNumber05,
       "Case Status:  Case closed",
     );
   });
