8745b2763fd9f0e6d99ead3d933bf05a345ff914
jessieharrigan <144362161+jessieharrigan@users.noreply.github.com>
2024-10-23 16:46:32 +0100

    Refactor postponeHearing

---
+++ src/tests/journeys/WA/postponeHearing.ts
@@ -0,0 +1,129 @@
+import { Page } from "@playwright/test";
+import commonHelpers, {
+  hearingPostponedReasons,
+} from "../../helpers/commonHelpers.ts";
+import postponeHearingSelectHearingPage from "../../pages/CaseAPI/postponeHearing/postponeHearingSelectHearingPage.ts";
+import postponeHearingReasonPage from "../../pages/CaseAPI/postponeHearing/postponeHearingReasonPage.ts";
+import postponeHearingNotifyPage from "../../pages/CaseAPI/postponeHearing/postponeHearingNotifyPage.ts";
+import submitPage from "../../pages/CaseAPI/postponeHearing/submitPage.ts";
+import confirmPage from "../../pages/CaseAPI/postponeHearing/confirmPage.ts";
+import hearingsTabPage from "../../pages/CaseAPI/caseTabs/hearingsTabPage.ts";
+import hearingTabPage from "../../pages/CaseAPI/caseTabs/hearingsTabPage.ts";
+
+type PostponeHearing = {
+  postponeHearing(
+    page: Page,
+    accessibilityTest: boolean,
+    reasonPostponed: hearingPostponedReasons,
+    errorMessaging: boolean,
+    caseNumber: string,
+    subjectName: string,
+  ): Promise<void>;
+};
+
+const postponeHearing: PostponeHearing = {
+  async postponeHearing(
+    page: Page,
+    accessibilityTest: boolean,
+    reasonPostponed: hearingPostponedReasons,
+    errorMessaging: boolean,
+    caseNumber: string,
+    subjectName: string,
+  ): Promise<void> {
+    await commonHelpers.chooseEventFromDropdown(
+      page,
+      "Hearings: Postpone hearing",
+    );
+    switch (errorMessaging) {
+      default:
+        await postponeHearingSelectHearingPage.checkPageLoads(
+          page,
+          caseNumber,
+          accessibilityTest,
+          subjectName,
+        );
+        const hearing =
+          await postponeHearingSelectHearingPage.fillInFields(page);
+        await postponeHearingSelectHearingPage.continueOn(page);
+        await postponeHearingReasonPage.checkPageLoads(
+          page,
+          caseNumber,
+          accessibilityTest,
+          subjectName,
+        );
+        await postponeHearingReasonPage.fillInFields(page, reasonPostponed);
+        await postponeHearingReasonPage.continueOn(page);
+        await postponeHearingNotifyPage.checkPageLoads(
+          page,
+          caseNumber,
+          accessibilityTest,
+          subjectName,
+        );
+        await postponeHearingNotifyPage.continueOn(page);
+        await submitPage.checkPageLoads(
+          page,
+          caseNumber,
+          accessibilityTest,
+          subjectName,
+        );
+        await submitPage.checkValidInfo(page, hearing, reasonPostponed);
+        await submitPage.continueOn(page);
+        await confirmPage.checkPageLoads(
+          page,
+          caseNumber,
+          accessibilityTest,
+          subjectName,
+        );
+        await confirmPage.continueOn(page);
+        await hearingsTabPage.changeToHearingsTab(page);
+        await hearingsTabPage.checkPageLoads(
+          page,
+          true,
+          false,
+          false,
+          "Birmingham Civil And Family Justice Centre-Priory Courts, 33 Bull Street",
+          false,
+          false,
+          null,
+          false,
+          false,
+          false,
+          true,
+          false,
+          accessibilityTest,
+        );
+        await hearingTabPage.checkValidInfoPostponeHearing(
+          page,
+          reasonPostponed,
+          "2-Midlands",
+          "Final",
+          "Paper",
+          "Morning",
+          "Birmingham Civil And Family Justice Centre-Priory Courts, 33 Bull Street",
+        );
+        break;
+      case true:
+        await postponeHearingSelectHearingPage.checkPageLoads(
+          page,
+          caseNumber,
+          accessibilityTest,
+          subjectName,
+        );
+        await postponeHearingSelectHearingPage.triggerErrorMessages(page);
+        await postponeHearingSelectHearingPage.fillInFields(page);
+        await postponeHearingSelectHearingPage.continueOn(page);
+        await postponeHearingReasonPage.checkPageLoads(
+          page,
+          caseNumber,
+          accessibilityTest,
+          subjectName,
+        );
+        await postponeHearingReasonPage.triggerErrorMessages(page);
+        await postponeHearingReasonPage.fillInFields(page, reasonPostponed);
+        await postponeHearingReasonPage.continueOn(page);
+        break;
+    }
+  },
+};
+
+export default postponeHearing;
--- src/tests/pages/CaseAPI/postponeHearing/postponeHearingSelectHearingPage.ts
+++ src/tests/pages/CaseAPI/postponeHearing/postponeHearingSelectHearingPage.ts
@@ -1,7 +1,6 @@
 import { expect, Page } from "@playwright/test";
 import axeTest from "../../../helpers/accessibilityTestHelper.ts";
 import postponeHearingSelectHearingContent from "../../../fixtures/content/CaseAPI/postponeHearing/postponeHearingSelectHearing_content.ts";
-import caseSubjectDetailsObject_content from "../../../fixtures/content/CaseAPI/createCase/caseSubjectDetailsObject_content.ts";
 import commonHelpers from "../../../helpers/commonHelpers.ts";
 
 type PostponeHearingSelectHearingPage = {
@@ -12,6 +11,7 @@ type PostponeHearingSelectHearingPage = {
     page: Page,
     caseNumber: string,
     accessibilityTest: boolean,
+    subjectName: string,
   ): Promise<void>;
   fillInFields(page: Page): Promise<string | null>;
   triggerErrorMessages(page: Page): Promise<void>;
@@ -27,6 +27,7 @@ const postponeHearingSelectHearingPage: PostponeHearingSelectHearingPage = {
     page: Page,
     caseNumber: string,
     accessibilityTest: boolean,
+    subjectName: string,
   ): Promise<void> {
     await page.waitForSelector(
       `.govuk-heading-l:text-is("${postponeHearingSelectHearingContent.pageTitle}")`,
@@ -35,9 +36,7 @@ const postponeHearingSelectHearingPage: PostponeHearingSelectHearingPage = {
       expect(page.locator(".govuk-caption-l")).toHaveText(
         postponeHearingSelectHearingContent.pageHint,
       ),
-      expect(page.locator("markdown > h3")).toContainText(
-        caseSubjectDetailsObject_content.name,
-      ),
+      expect(page.locator("markdown > h3")).toContainText(subjectName),
       expect(page.locator("markdown > p").nth(0)).toContainText(
         postponeHearingSelectHearingContent.caseReference + caseNumber,
       ),
