3f0436ac95c25a37eb0e5d81bbb63edb55f1856a
jessieharrigan <144362161+jessieharrigan@users.noreply.github.com>
2024-11-18 10:01:25 +0000

    Change to nightly definition

--- src/tests/E2E/globalTestCleanUp.test.ts
+++ src/tests/E2E/globalTestCleanUp.test.ts
@@ -6,7 +6,7 @@ test.describe("Global Data Cleanup. @dataCleanup", () => {
   test("Global automation test data clean up. - Admin", async ({
     page,
   }): Promise<void> => {
-    test.setTimeout(10 * 60 * 1000);
+    test.setTimeout(15 * 60 * 1000);
     await globalDataCleanUp.globalDataCleanUp(
       page,
       waUsers_content.userRoleAdmin,
@@ -16,7 +16,7 @@ test.describe("Global Data Cleanup. @dataCleanup", () => {
   test("Global automation test data clean up. - Judge", async ({
     page,
   }): Promise<void> => {
-    test.setTimeout(10 * 60 * 1000);
+    test.setTimeout(15 * 60 * 1000);
     await globalDataCleanUp.globalDataCleanUp(
       page,
       waUsers_content.userRoleJudge,
@@ -26,14 +26,14 @@ test.describe("Global Data Cleanup. @dataCleanup", () => {
   test("Global automation test data clean up. - Legal Officer", async ({
     page,
   }): Promise<void> => {
-    test.setTimeout(10 * 60 * 1000);
+    test.setTimeout(15 * 60 * 1000);
     await globalDataCleanUp.globalDataCleanUp(page, waUsers_content.userRoleLO);
   });
 
   test("Global automation test data clean up. - Caseworker", async ({
     page,
   }): Promise<void> => {
-    test.setTimeout(10 * 60 * 1000);
+    test.setTimeout(15 * 60 * 1000);
     await globalDataCleanUp.globalDataCleanUp(
       page,
       waUsers_content.userRoleCaseWorker,
