73e9cd21fec0e4b2ff8f5f7c7f2745273e77c566
Will Orchard <will.orchard@solirius.com>
2025-08-27 15:58:17 +0100

    Add changes

--- src/tests/fixtures/content/DSSCreateCase/cicaReference_content.ts
+++ src/tests/fixtures/content/DSSCreateCase/cicaReference_content.ts
@@ -6,9 +6,9 @@ const cicaReferenceContent = {
   button: "Continue",
   errorBanner: "There is a problem",
   selectionError: "Enter CICA reference number",
-  pageTitleCy:
-    "Cyfeirnod Awdurdod Digolledu am Anafiadau Troseddol",
-  hintTextCy1: "Gallwch ddod o hyd i hwn ar unrhyw ohebiaeth rydych wedi'i derbyn gan ADAT.",
+  pageTitleCy: "Cyfeirnod Awdurdod Digolledu am Anafiadau Troseddol",
+  hintTextCy1:
+    "Gallwch ddod o hyd i hwn ar unrhyw ohebiaeth rydych wedi'i derbyn gan ADAT.",
   subHeadingCy1: "Cyfeirnod ADAT",
   buttonCy: "Parhau",
   errorBannerCy: "Mae yna broblem",
