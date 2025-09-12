98a3814173eef06ca75cb96d4b6695045aa08e81
Will Orchard <will.orchard@solirius.com>
2025-09-12 12:01:16 +0100

    Wording fix

--- src/tests/fixtures/content/DSSCreateCase/cicaReference_content.ts
+++ src/tests/fixtures/content/DSSCreateCase/cicaReference_content.ts
@@ -1,12 +1,12 @@
 const cicaReferenceContent = {
-  pageTitle: "Criminal Injuries Compensation Authority Reference Number",
+  pageTitle: "Enter your Criminal Injuries Compensation Authority Reference Number",
   hintText1:
     "You can find this on any correspondence that you've received from CICA, CICA reference numbers begin with the letter X or G",
   subHeading1: "CICA reference number",
   button: "Continue",
   errorBanner: "There is a problem",
   selectionError: "Enter CICA reference number",
-  pageTitleCy: "Cyfeirnod Awdurdod Digolledu am Anafiadau Troseddol",
+  pageTitleCy: "Nodwch eich Cyfeirnod Awdurdod Digolledu am Anafiadau Troseddol",
   hintTextCy1:
     "Gallwch ddod o hyd i hwn ar unrhyw ohebiaeth rydych wedi'i derbyn gan ADAT, Mae cyfeirnodau ADAT yn dechrau gyda’r lythyren X neu G",
   subHeadingCy1: "Cyfeirnod ADAT",
