77dc63b2dbaa44945d18cd9492bb5e219153cb49
Will Orchard <will.orchard@solirius.com>
2026-02-18 12:05:00 +0000

    frontend create case journey

---
+++ src/tests/fixtures/content/DSSCreateCase/cicaDecisionDate_content.ts
@@ -0,0 +1,24 @@
+const cicaDecisionDateContent = {
+  pageTitle:
+    "Enter the date of your Criminal Injuries Compensation Authority initial review decision letter",
+  hintText1:
+    "You can find this on your CICA initial review decision letter",
+  subHeading1: "Date of CICA initial review decision letter",
+  button: "Continue",
+  errorBanner: "There is a problem",
+  dateError: "Date of CICA initial review decision letter must include a day, month and year",
+  validDateError: "Date of CICA initial review decision letter must be a real date and must include a day, month and year",
+  inlineValidDateError: "Date of CICA initial review decision letter must be a real date and must include a day, month and year",
+  pageTitleCy:
+    "Rhowch ddyddiad eich llythyr penderfyniad adolygu cychwynnol yr Awdurdod Digolledu am Anafiadau Troseddol",
+  hintTextCy1:
+    "Gallwch ddod o hyd iddo ar eich llythyr penderfyniad adolygu cychwynnol ADAT",
+  subHeadingCy1: "Dyddiad llythyr penderfyniad adolygu cychwynnol ADAT",
+  buttonCy: "Parhau",
+  errorBannerCy: "Mae yna broblem",
+  dateErrorCy: "Rhaid i'r dyddiad llythyr penderfyniad adolygu cychwynnol ADAT gynnwys diwrnod, mis a blwyddyn",
+  validDateErrorCy: "Rhaid i'r dyddiad llythyr penderfyniad adolygu cychwynnol ADAT fod yn ddyddiad go iawn a rhaid iddo gynnwys diwrnod, mis a blwyddyn",
+  inlineValidDateErrorCy: "Rhaid i'r dyddiad llythyr penderfyniad adolygu cychwynnol ADAT fod yn ddyddiad go iawn a rhaid iddo gynnwys diwrnod, mis a blwyddyn",
+} as const;
+
+export default cicaDecisionDateContent;
