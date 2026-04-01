00ae35767fea39b494c1454f05cfd8f785c9cfe4
Will Orchard <will.orchard@solirius.com>
2026-04-01 10:57:57 +0100

    Update function to fill in yesterday's date

--- src/tests/pages/DSSCreateCase/cicaDecisionDatePage.ts
+++ src/tests/pages/DSSCreateCase/cicaDecisionDatePage.ts
@@ -75,9 +75,11 @@ const cicaDecisionDatePage: CicaDecisionDatePage = {
 
   async fillInFields(page: Page, decisionDate?: Date): Promise<void> {
     const currentDate = decisionDate ?? new Date();
-    await page.fill(this.day, `${currentDate.getDate() - 1}`);
-    await page.fill(this.month, `${currentDate.getMonth() + 1}`);
-    await page.fill(this.year, `${currentDate.getFullYear()}`);
+    const yesterday = new Date(currentDate);
+    yesterday.setDate(currentDate.getDate() - 1);
+    await page.fill(this.day, `${yesterday.getDate()}`);
+    await page.fill(this.month, `${yesterday.getMonth() + 1}`);
+    await page.fill(this.year, `${yesterday.getFullYear()}`);
     await Promise.all([
       page.waitForLoadState("domcontentloaded"),
       page.click(this.continue),
