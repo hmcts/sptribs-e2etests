f3bab6540bf2e074507c377b59781837184f4b88
Will Orchard <will.orchard@solirius.com>
2026-08-12 11:15:43 +0100

    Update sign in for manage-cases

--- src/tests/pages/DSSUpdateCase/loginPage.ts
+++ src/tests/pages/DSSUpdateCase/loginPage.ts
@@ -9,7 +9,7 @@ type SignInPage = {
 
 const signInPage: SignInPage = {
   async SignInUser(page: Page): Promise<void> {
-    await idamLoginHelper.signInUserIDAM(
+    await idamLoginHelper.signInUserDSS(
       page,
       waUsers_content.userRoleCitizen,
       config.UpdateCaseBaseURL,
